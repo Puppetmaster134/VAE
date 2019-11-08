232cb0a44a9705cc2e48e5f7b45cbb0181c378f0
brian <bzimmerman@csumb.edu>
2019-11-07 19:44:12 -0500

    Parameterized and commented everything

--- train.py
+++ train.py
@@ -11,12 +11,12 @@ from vae import VAE
 
 
 parser = argparse.ArgumentParser(description='VAE MNIST Example')
+parser.add_argument('--model',default='VAE',help='Name of model')
 parser.add_argument('--batch-size', type=int, default=128, metavar='N',help='input batch size for training (default: 128)')
 parser.add_argument('--epochs', type=int, default=10, metavar='N',help='number of epochs to train (default: 10)')
 parser.add_argument('--no-cuda', action='store_true', default=False,help='enables CUDA training')
 parser.add_argument('--seed', type=int, default=1, metavar='S',help='random seed (default: 1)')
 parser.add_argument('--log-interval', type=int, default=10, metavar='N',help='how many batches to wait before logging training status')
-parser.add_argument('--train', action='store_true', default=False, help='determines whether we want to train or infer')
 args = parser.parse_args()
 
 # Set up CUDA if it's not disabled via argument
@@ -25,55 +25,63 @@ device = torch.device("cuda" if args.cuda else "cpu")
 kwargs = {'num_workers': 1, 'pin_memory': True} if args.cuda else {}
 
 MODEL_STORE_PATH = 'C:\\Users\\Brian\\Documents\\Projects\\vae\\Models\\'
-MODEL_NAME = "VAE.ckpt"
-
-
+MODEL_NAME = f"{args.model}.ckpt"
 
 torch.manual_seed(args.seed)
 
 # Set up our training and testing sets (MNIST)
 # 60000 elements
-train_loader = torch.utils.data.DataLoader(
-    datasets.MNIST('../data', train=True, download=True,
-                   transform=transforms.ToTensor()),
-    batch_size=args.batch_size, shuffle=True, **kwargs)
-	
+
+train_dataset = datasets.MNIST('../data', train=True, download=True, transform=transforms.ToTensor())
+train_loader = torch.utils.data.DataLoader(train_dataset,batch_size=args.batch_size, shuffle=True, **kwargs)
+    
+num_items = train_dataset.data.size(0)
+image_height = train_dataset.data.size(1)
+image_width = train_dataset.data.size(2)
+latent_features = 20
+
+
 # Initialize our model
-model = VAE(20).to(device)
+model = VAE(latent_features, image_height, image_width).to(device)
+
 
 # Setup Adam optimizer
 optimizer = optim.Adam(model.parameters(), lr=1e-3)
 
-total_loss = 0
-num_epochs = 200
-ce = 0
-kld = 0
-
-for epoch in range(0,num_epochs):
+for epoch in range(0,args.epochs):
+    total_loss = 0
     if __name__ == '__main__':
         for batch_idx, (data, _) in enumerate(train_loader):
+            
+            #Moves our data to our primary device (CPU or GPU)
             data = data.to(device)
+            
+            #Reset the gradients
             optimizer.zero_grad()
+            
+            #Our model returns the reconstruction, the input, and our encoder hidden state as means and variances
             x_prime, x, mean, variance = model(data)
-            cross_entropy_loss, kl_divergence_loss = model.loss_function(x_prime,x,mean,variance)
             
+            #Get the Binary Cross Entropy and KL Divergence
+            BCE, KLD = model.loss_function(x_prime,x,mean,variance)
             
-            loss = cross_entropy_loss + kl_divergence_loss
+            #Add up Binary Cross Entropy and KL Divergence to get our loss
+            loss = BCE + KLD
+            
+            #Backpropagation!
             loss.backward()
             total_loss += loss.item()
             optimizer.step()
             
-            
+            #Some print stuff
             if batch_idx % args.log_interval == 0:
                 print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
                 epoch, batch_idx * len(data), len(train_loader.dataset),
                 100. * batch_idx / len(train_loader),
                 loss.item() / len(data)))
-            
-        #print(ce)
-    #print('tl', total_loss)
 
-torch.save(model.state_dict(), f'{MODEL_STORE_PATH}/{MODEL_NAME}')
+#Save the model
+#torch.save(model.state_dict(), f'{MODEL_STORE_PATH}/{MODEL_NAME}')
 	
 	
 	
--- vae.py
+++ vae.py
@@ -7,44 +7,70 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 class VAE(nn.Module):
-	def __init__(self, hidden_size):
+	def __init__(self, hidden_size, image_height, image_width):
 		super(VAE, self).__init__()
 
+		#Number of input/output features
+		self.output_features = image_height * image_width
+		
+		#Number of channels ConvNet will extract features with
+		self.conv_channels = 3
+		
+		#The size of z (encoded variables)
+		self.hidden_size = hidden_size
+		
+		#First Convolutional Layer Parameters
+		self.conv1_size = 4
+		self.conv1_stride = 1
+		self.pool1_size = 2
+		self.pool1_stride = 2
+		
+		#Second Convolutional Layer Parameters
+		self.conv2_size = 2
+		self.conv2_stride = 1
+		self.pool2_size = 2
+		self.pool2_stride = 1
+		
+		#Calculating the number of features extracted from our convolutional sequence
+		conv1_output_size = self.calculate_conv_output(image_height,self.conv1_size, self.conv1_stride)
+		pool1_output_size = self.calculate_pool_output(conv1_output_size, self.pool1_size, self.pool1_stride)
+		conv2_output_size = self.calculate_conv_output(pool1_output_size,self.conv2_size, self.conv2_stride)
+		pool2_output_size = self.calculate_pool_output(conv2_output_size, self.pool2_size, self.pool2_stride)
+		seq_output_size = np.floor(pool2_output_size)
+		self.conv_features = int(np.square(seq_output_size) * self.conv_channels)
 
+		#Sequential Layer with the Convolutional Layers inside
 		self.conv = nn.Sequential(
 			nn.Conv2d(
 				in_channels=1,
-				out_channels=3,
-				kernel_size=(4,4),
-				stride=1),
+				out_channels=self.conv_channels,
+				kernel_size=(self.conv1_size, self.conv1_size),
+				stride=self.conv1_stride),
 			nn.ReLU(True),
-			nn.MaxPool2d(2,stride=2),
+			nn.MaxPool2d(self.pool1_size,stride=self.pool1_stride),
 			nn.Conv2d(
-				in_channels=3,
-				out_channels=3,
-				kernel_size=(2,2),
-				stride=1),
+				in_channels=self.conv_channels,
+				out_channels=self.conv_channels,
+				kernel_size=(self.conv2_size,self.conv2_size),
+				stride=self.conv2_stride),
 			nn.ReLU(True),
-			nn.MaxPool2d(2,stride=1)
+			nn.MaxPool2d(self.pool2_size,stride=self.pool2_stride)
 		)
+		
+		self.encoder_fc1 = nn.Linear(self.conv_features,self.hidden_size)
+		self.encoder_fc2 = nn.Linear(self.conv_features,self.hidden_size)
 
-		self.encoder_fc1 = nn.Linear(300,20)
-		self.encoder_fc2 = nn.Linear(300,20)
-
-		self.decode_fc1 = nn.Linear(20,300)
-		self.decode_fc2 = nn.Linear(300,784)
+		self.decode_fc1 = nn.Linear(self.hidden_size,self.conv_features)
+		self.decode_fc2 = nn.Linear(self.conv_features,self.output_features)
 
 	def encode(self,input):
-		# input = (batch_size * input_channels * input_height * input_width)
-		h = self.conv(input).view(-1,300)
+		h = self.conv(input).view(-1,self.conv_features)
 		return self.encoder_fc1(h), self.encoder_fc2(h)
 
 	def reparameterize(self, mean, variance):
 		std_dev = torch.exp(variance / 2)
 		epsilon = torch.randn_like(std_dev)
-
 		z = mean + epsilon * std_dev
-
 		return z
 
 	def decode(self, z):
@@ -52,27 +78,32 @@ class VAE(nn.Module):
 		return torch.sigmoid(self.decode_fc2(h1))
 
 	def forward(self,input):
-		#normal distributions for each feature
+		#Means and Variances for each hidden feature
 		mean,variance = self.encode(input)
 
-		#Latent code z
+		#Latent State as sampled values (aka. z)
 		z = self.reparameterize(mean,variance)
 
-		#output constructed from z
+		#The output we decoded from z
 		x_prime = self.decode(z)#.view(-1,1,28,28)
 
 		return x_prime, input, mean, variance
 
-	# Reconstruction + KL divergence losses summed over all elements and batch
-	def loss_function(self,recon_x, x, mu, logvar):
-		BCE = F.binary_cross_entropy(recon_x, x.view(-1, 784), reduction='sum')
-
-		# see Appendix B from VAE paper:
-		# Kingma and Welling. Auto-Encoding Variational Bayes. ICLR, 2014
-		# https://arxiv.org/abs/1312.6114
-		# 0.5 * sum(1 + log(sigma^2) - mu^2 - sigma^2)
-
+	
+	def loss_function(self,recon_x, x, mu, logvar):		
+		#Binary Cross Entropy loss function
+		BCE = F.binary_cross_entropy(recon_x, x.view(-1, self.output_features), reduction='sum')
+		
+		#Kullback-Leibler (KL) Divergence loss function
+		#Calculates the loss between our encoded distribution and the Normal distribution
 		KLD = -0.5 * torch.sum(1 + logvar - mu.pow(2) - logvar.exp())
 
-
 		return BCE, KLD
+	
+	#Given some parameters, calculate the number of features returned by a ConvLayer
+	def calculate_conv_output(self, input_width, kernel_width, stride = 1, padding_size = 0):
+		return ((input_width + (2 * padding_size) - kernel_width) / stride) + 1
+	
+	#Given some parameters, calculate the number of features returned by a Pooling Layer
+	def calculate_pool_output(self, input_width, kernel_width, stride = 1):
+		return ((input_width - kernel_width) / stride) + 1
